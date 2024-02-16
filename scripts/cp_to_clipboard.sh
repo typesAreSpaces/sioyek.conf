ae9fbf6aa7d252aadb7fbb86fd8886122fc0836f
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2024-02-16 12:18:08 -0700

    Fix scripts.

--- scripts/cp_to_clipboard.sh
+++ scripts/cp_to_clipboard.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-FILE_PATH=$(dirname $1)
+FILE_PATH=$(dirname "$1")
 FILE_PATH=${FILE_PATH:1}
-echo ${FILE_PATH} | pbcopy
+echo "${FILE_PATH}" | pbcopy
--- scripts/open_in_terminal.sh
+++ scripts/open_in_terminal.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-FILE_PATH=$(dirname $1)
+FILE_PATH=$(dirname "$1")
 FILE_PATH=${FILE_PATH:1}
-/opt/homebrew/bin/alacritty --working-directory ${FILE_PATH}
+/opt/homebrew/bin/alacritty --working-directory "${FILE_PATH}"
